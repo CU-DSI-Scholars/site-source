c15c3b640024c89fa5cdd59e1476b7491fcb2e16
Vincent Dorie <vdorie@gmail.com>
2021-01-04 15:11:08 -0500

    Student call template

--- readme.md
+++ readme.md
@@ -16,9 +16,65 @@ DSI & DFG Scholars Program, Implementation Details
 5. Faculty/student interviews and selection (+4 weeks)
 6. Stipend information due (+1 week)
 
+## Sending Emails
+
+Gmail drafts are created using the [gmailr](https://github.com/r-lib/gmailr) package for R. This requires configuring Gmail itself to permit API access and downloading a set of credentials files to the local compute. Steps for this can be found [here](https://github.com/r-lib/gmailr#setup). Note that the credentials file should not be checked into the repository. By default, this file is expected to sit in the `site-source` folder (see below).
+
+## Maintaining the Website
+
+The website is comprised of two repositories, `[site-source](https://github.com/CU-DSI-Scholars/site-source)` and `[DSI-scholars](https://github.com/CU-DSI-Scholars/DSI-scholars)`. The first contains the content used to build the second, which is the website itself. In order to to get started, check out both repositories so that they sit in the same root directory. For example:
+
+```
+mkdir ~/Documents/dsi_scholars
+cd ~/Documents/dsi_scholars
+git clone https://github.com/CU-DSI-Scholars/site-source
+git clone https://github.com/CU-DSI-Scholars/DSI-scholars
+```
+
+The website itself is built from `site-source` using the [blogdown](https://github.com/rstudio/blogdown) package. The general workflow is:
+
+1. Modify `site-source` in some way.
+   1. Check in the change into the repository (`git add` new files; `git commit -m '` commit message `'`; `git push`)
+2. Load `site-source.Rproj` in Rstudio or start a base R session and set the working directory to `PATH/TO/site-source`
+3. Run `blogdown::build_site()` from within R; this will update 
+4. Check in the changes to the `DSI-scholars` repository using `git` add/commit/push
+
+## Structure of the Repository
+
+The following paths and files in `site-source` are important:
+
+* `config.toml` - contains overall settings for the site including the url for the "Apply Now" button; change this to the new student application form as necessary
+* `content/page` - static pages, currently used for FAQs and overview of DSI programs
+* `content/post` - individual blog "posts", in particular the project pages
+* `static` - anything in this folder gets verbatim copied to the website; it is currently used to stash images (not needed at all) and to host pdfs/docs that are specific to individual projects
+
+### Static URLs
+
+In addition, two files will always be reachable through specific URLs by using HTTP redirects. As of writing, these are:
+
+* `content/post/pi_faq_2021_spring_summer.md` - the alias line in this file means that it can always be reached at (/static/pi_faq)[https://cu-dsi-scholars.github.io/DSI-scholars/static/pi_faq/]
+* `content/page/2020-12-09-call-for-faculty-participation-dsi-scholars-spring-summer-2021.md` is aliased to (/static/pi_call)[https://cu-dsi-scholars.github.io/DSI-scholars/static/pi_call/]
+
+When a new FAQ or faculty call are released, or if additional static URLs are desired, these files will have to be edited to remove their alias blocks and those blocks added elsewhere.
+
+## Clip Art
+
+Currently, clip art is hosted on the free image hosting site [Cloudinary](https://cloudinary.com/). It is not necessary to use this, but the site does have some advantages. The structure of a Cloudinary link has some useful information:
+
+* `https://res.cloudinary.com/vdoriecu/image/upload/c_thumb,w_200,g_face/v1599765419/social_network_bskhfi.png`
+* `https://res.cloudinary.com/USERNAME/image/upload/TRANSFORMATION/FILEID/FILENAME.png`
+
+For the most part, when a new piece of clip art is uploaded it is sufficient to copy an old URL, if necessary change the user name, and change just the file id and file name component. This allows you to re-use the existing transformation, most of which are necessary to turn in the image into a thumbnail of the appropriate size. You an optionally adjust that on Cloudinary itself.
+
+## URL Shortening
+
+URL shortening is done through [bit.ly](bit.ly). It is only truly necessary for the application forms, as Google forms does not allow HTML (maybe you can copy/paste, never tried).
+
 ## Faculty Call
 
-1. Create an application form
+1. Create and update an application form by cloning the previous [one](https://docs.google.com/forms/d/1804fmD_dkfSvNY3MGkIf2F3oQZzHuFWLbdSHi1Qc73Y/edit)
+   * Banner image is created by modifying [static/img/opencall_faculty2.xcf] using [GIMP](https://www.gimp.org/)
+   * It can be helpful to also look at the call from the (year previous)[https://docs.google.com/forms/d/1G2oDkj4_6B8pHeyyowdCaB3OycboLoqNM4KXqFl99Ys/edit?usp=drive_web], to see if any term-specific language needs to be included
 2. [Draft](scripts/faculty_call_template.R) an announcement and send it to Jonathan
 
 ## Funding Decision
@@ -37,5 +93,13 @@ DSI & DFG Scholars Program, Implementation Details
    2. Run the page creation [script](scripts/project_page_skeleton.R)
 3. Clone and update the student application form ([previous term's](https://docs.google.com/forms/d/1M-SVYlR1dKw3plAoJ1rxspTBRunMR-4QutycMEa7jAo/edit))
    * Because the language between spring/summer and fall changes, it can be useful to also refer to last [spring/summer's](https://docs.google.com/forms/d/1xYos2eQwoEoiYzYjiHi7U4ll9RSSmQ2P4KaKZwZ032g/edit?usp=drive_web)
+   * See the step below
 4. Email faculty regarding decisions by running the email [script](scripts/faculty_submission_response.R)
 
+Once the emails have been sent, update the [tags](https://cu-dsi-scholars.github.io/DSI-scholars/tags/) and clip art for projects.
+
+## Student Application Form and Call
+
+1. Clone and update the previous student application [page/2020-09-09-call-for-student-applications-dsi-dfg-scholars-fall-2020](information page).
+2. Clone and update the previous [https://docs.google.com/forms/d/1M-SVYlR1dKw3plAoJ1rxspTBRunMR-4QutycMEa7jAo/edit](application form).
+3. Schedule an online information session (previous [https://docs.google.com/presentation/d/11KPHFM94AcjEuTAZRP7qDmFmQ9UOYkK2EYHZY3bemyU/edit](slides))
---
+++ scripts/student_call_template.R
@@ -0,0 +1,131 @@
+# This file creates a draft of the call for faculty participation. In the past,
+# it has been forwarded to Jonathan and/or Tian to be circulated among the
+# faculty. With the addition of some DSI communication staff, it will likely
+# need to modified before it can be sent out so it should be prepared a few days
+# before you would like it to be sent.
+
+### VARIABLES TO UPDATE #####
+# Everything from here to VARIABLES END will likely need to be changed.
+
+# URL to banner. In previous years we used a modified DSI Scholars logo with the
+# submission date added, but the communications team has removed that and just
+# used the base version.
+call_banner_url <- 
+  "https://cu-dsi-scholars.github.io/DSI-scholars/img/opencall_students_fall_2020.png"
+
+# These two can likely be turned into aliased urls on the website to minimize
+# maintenance
+student_call_url <-
+  "https://cu-dsi-scholars.github.io/DSI-scholars/2020/09/call-for-student-applications-dsi-scholars-fall-2020/"
+student_faq_url <-
+  "https://cu-dsi-scholars.github.io/DSI-scholars/page/faq_fall_2020/"
+info_session_url <-
+  "https://columbiauniversity.zoom.us/meeting/register/tJIlf-muqzMtEtZOPVAXBFgSpuY6RqwEKO0g"
+
+info_session_time <-
+  "11:00am Eastern time on September 24th."
+
+apply_now_img_url <-
+  "https://res.cloudinary.com/vdoriecu/image/upload/v1570330765/apply-now-button_blue_pvjmkq.png"
+
+year <- 2021
+term <- "Spring/Summer"
+
+application_deadline <- as.Date("2021-01-29")
+application_deadline_short <- format(application_deadline, "%m/%d/%Y")
+application_deadline_long <- toupper(format(application_deadline, "%B %d, %Y"))
+application_deadline_bold <- toupper(application_deadline_long)
+
+# Where the students can apply.
+application_url <-
+  "https://docs.google.com/forms/d/e/1FAIpQLSe3Rxxf9PVZ5IbuRoyy5slMz7kcC9-4ILWPPnyyWNV6nacppg"
+
+
+# Credentials file created from gmail; set to where the file can be found.
+# Info on creating it can be found at https://github.com/r-lib/gmailr#setup
+credentials_file <- file.path("gmail_credentials.json")
+
+# Configure as desired.
+email_to <- "Vincent Dorie <vjd2106@columbia.edu>"
+email_from <- "Vincent Dorie <vjd2106@columbia.edu>"
+
+email_title <- glue("
+  Data Science Institute {term} Scholars Programs | Application Deadline {application_deadline_short}"
+
+### VARIABLES END ###
+
+if (!require(gmailr, quietly = TRUE)) {
+  remotes::install_github("r-lib/gmailr")
+  library(gmailr, quietly = TRUE)
+}
+library(glue, quietly = TRUE)
+
+email_body <- glue("
+  <center>
+  <a href=\"{application_url}\">
+  <img src=\"{call_banner_url}\" width=\"479\" height=\"346\">
+  </a>
+  <h1 style=\"color:#0072CE;\">DSI Scholars Program</h1>
+  <h2>Call for Student Applications | {term} {year} Cohort</h2>
+  <h2 style=\"color::#FF0000\">DEADLINE TO APPLY: {application_deadline_bold}</h2>
+  </center>
+  <p>The <a href=\"http://datascience.columbia.edu/\">Data Science Institute\\
+  </a> is pleased to announce that the Data Science Institute (DSI) and Data \\
+  For Good Scholars Programs for <a href=\"{student_call_url}\">{term} {year}\\
+  </a> are open for applications.</p>
+  <p>The goal of the DSI Scholars Program is to engage Columbia's \\
+  undergraduate and master students in data science research with Columbia \\
+  faculty through a research internship. The program connects students with \\
+  research projects across Columbia and provides student researchers with \\
+  additional learning experience and networking opportunities. Through unique \\
+  enrichment activities, this program aims to foster a learning and \\
+  collaborative community in data science at Columbia.</p>
+  <p>The Data For Good Scholars program connects student volunteers to \\
+  organizations and individuals working for the social good whose projects \\
+  have developed a need for data science expertise. As \"real world\" \\
+  problems with real world data, these projects are excellent opportunities \\
+  for students to learn how data science is practiced outside of the \\
+  university setting and to learn how to work effectively with people for \\
+  whom data science sits outside of their subject area.</p>
+  <p>This term we have had a very large number of projects submitted; please \\
+  give yourself enough time to read the project descriptions. For your \\
+  application, choose the <span style=\"background-color:#FFFF00\";\">TOP five \\
+  projects of which you are most interested, eligible and qualified.</span></p>
+  <p>Please email questions to <a href=\"mailto:dsi-scholars@columbia.edu\">\\
+  dsi-scholars@columbia.edu</a>, see our <a href=\"{student_faq_url}\">FAQ</a>, \\
+  or <a href=\"{info_session_url\">register</a> for the information session \\
+  at {info_session_time}.</p>
+  <center>
+  <p><strong>The Data Science Institute encourages women and \\
+  underrepresented minorities to apply to these programs.</strong></p>
+  <h3>Applications are due by {application_deadline_long}.</h3>
+  <p><a href=\"application_url\"><img src=\"{apply_now_img_url}\"></a></p>
+  </center>")
+
+if (!require(gmailr, quietly = TRUE)) {
+  remotes::install_github("r-lib/gmailr")
+  require(gmailr, quietly = TRUE)
+}
+
+gm_auth_configure(path = credentials_file)
+
+email <- gm_mime(to = email_to,
+                 from = email_from,
+                 subject = email_title) %>%
+  gm_html_body(email_body, content_type = "text/html", charset = "utf-8")
+
+gm_create_draft(email)
+
+
+
+Call for Student Applications
+Columbia University Data Science Institute is pleased to announce that the Data Science Institute (DSI) and Data For Good Scholars Programs for Fall 2020 are open for applications.
+The goal of the DSI Scholars Program is to engage Columbia's undergraduate and master students in data science research with Columbia faculty through a research internship. The program connects students with research projects across Columbia and provides student researchers with additional learning experience and networking opportunities. Through unique enrichment activities, this program aims to foster a learning and collaborative community in data science at Columbia.
+The Data For Good Scholars program connects student volunteers to organizations and individuals working for the social good whose projects have developed a need for data science expertise. As "real world" problems with real world data, these projects are excellent opportunities for students to learn how data science is practiced outside of the university setting and to learn how to work effectively with people for whom data science sits outside of their subject area.
+This year we have had an unprecedented number of projects; please give yourself enough time to read the project descriptions. For your application choose the TOP five projects of which you are most interested, eligible and qualified.
+Please email questions to dsi-scholars@columbia.edu, see our FAQ, or register for the information session at 11:00am Eastern time on September 24th.
+The Data Science Institute encourages women and underrepresented minorities to apply to these programs.
+
+Applications are due by October 1st, 2020.
+
+ 
